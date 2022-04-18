5a558010c5ba4431fb5d1836b32d0cf35178a2f1
Matteo Leclercq <matteo.ftt@gmail.com>
2022-04-18 17:26:05 +0200

    Init from https://github.com/MatteoL-W/SDL2_Boilerplate

---
+++ include/interfaces/MenuInterface.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../Game.hpp"
+#include "../Interface.hpp"
+#include "../Menu.hpp"
+
+class MenuInterface : public Interface {
+public:
+    explicit MenuInterface(Game *game)
+            : game(game), menu(new Menu()) {
+    }
+
+    ~MenuInterface() = default;
+
+    void handleEvents() override;
+
+    void update() override;
+
+    void render() override;
+
+private:
+    Game *game;
+
+    Menu *menu;
+
+};
+
---
+++ src/interfaces/MenuInterface.cpp
@@ -0,0 +1,34 @@
+#include <SDL2/SDL.h>
+
+#include "../../include/interfaces/MenuInterface.hpp"
+
+/**
+ * @brief Handle SDL Events in the menu
+ */
+void MenuInterface::handleEvents() {
+    SDL_Event event = game->event;
+    SDL_PollEvent(&event);
+
+    if (event.type == SDL_QUIT) {
+        game->setRunning(false);
+    }
+
+}
+
+/**
+ * @brief Update the menu
+ */
+void MenuInterface::update() {
+    menu->update();
+}
+
+/**
+ * @brief Render the menu
+ */
+void MenuInterface::render() {
+    SDL_RenderClear(Game::renderer);
+
+    menu->draw();
+
+    SDL_RenderPresent(Game::renderer);
+}
