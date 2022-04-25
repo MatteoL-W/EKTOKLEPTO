02bcf465beb1afe6cefbdbf57f84b5a5b92f504f
MatteoL-petitordi <matteo.ftt@gmail.com>
2022-04-25 23:08:10 +0200

    début quadtree

--- include/tools/QuadTreeNode.hpp
+++ include/tools/QuadTreeNode.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include "../Box.hpp"
+#include "../tools/utils.hpp"
 
 
 struct QuadTreeNode {
@@ -10,16 +11,21 @@ struct QuadTreeNode {
     QuadTreeNode* bottomLeft;
     QuadTreeNode* bottomRight;
 
-    glm::vec2 TLCoord;
-    glm::vec2 BRCoord;
+    glm::vec2 TLQuad;
+    glm::vec2 BRQuad;
+    glm::vec2 centerQuad;
 
     std::vector<Box*> boxes;
 
-    void init() {
+    void init(glm::vec2 TLPosition, glm::vec2 BRPosition) {
         topLeft = nullptr;
         topRight = nullptr;
         bottomLeft = nullptr;
         bottomRight = nullptr;
+
+        TLQuad = TLPosition;
+        BRQuad = BRPosition;
+        centerQuad = getPositionCenter(TLPosition, BRPosition);
     }
 
     bool isLeaf() {
@@ -27,7 +33,99 @@ struct QuadTreeNode {
     }
 
     void insertBox(Box* box) {
+        // Si c'est une leaf
+        // On ajoute la box dans le vecteur de box associé
+        if (isLeaf() && boxes.size() < 4) {
+            boxes.push_back(box);
+            return;
+        }
 
+        // Si c'est pas une leaf
+        // On boucle sur les coins et on check ils sont dans quels zones
+        // On envoie la box dans les zones touchées
+        if (isLeaf() && boxes.size() > 4) {
+            for (size_t i = 0; i < boxes.size(); i++) {
+                bool left = false;
+                bool top = false;
+
+                // Là j'ai une box : boxes[i]
+                // Je mets la box dans l'enfant(s) qui correspond
+                if (boxes[i]->getTLPosition().x < centerQuad.x) {
+                    // une partie est à gauche
+                    left = true;
+                }
+
+                if (boxes[i]->getTRPosition().x > centerQuad.x) {
+                    // une partie est à droite
+                    left = false;
+                }
+
+                if (boxes[i]->getBRPosition().y < centerQuad.y) {
+                    // une partie est en dessous
+                    top = false;
+                }
+
+                if (boxes[i]->getTLPosition().y > centerQuad.y) {
+                    // une partie est au dessus
+                    top = true;
+                }
+
+                if (top && left) {
+                    if (!topLeft)
+                        topLeft->init(TLQuad, centerQuad);
+                    topLeft->insertBox(boxes[i]);
+                } else if (top) {
+                    if (!topRight)
+                        topRight->init(glm::vec2(centerQuad.x, TLQuad.y), glm::vec2(BRQuad.x, centerQuad.y));
+                    topRight->insertBox(boxes[i]);
+                } else if (left) {
+                    if (!bottomLeft)
+                        bottomLeft->init(glm::vec2(TLQuad.x, centerQuad.y), glm::vec2(centerQuad.x, BRQuad.y));
+                    bottomLeft->insertBox(boxes[i]);
+                } else {
+                    if (!bottomRight)
+                        bottomRight->init(centerQuad, BRQuad);
+                    bottomRight->insertBox(boxes[i]);
+                }
+            }
+        }
+
+        if (!isLeaf()) {
+            bool left = false;
+            bool top = false;
+
+            // Là j'ai une box : boxes[i]
+            // Je mets la box dans l'enfant(s) qui correspond
+            if (box->getTLPosition().x < centerQuad.x) {
+                // une partie est à gauche
+                left = true;
+            }
+
+            if (box->getTRPosition().x > centerQuad.x) {
+                // une partie est à droite
+                left = false;
+            }
+
+            if (box->getBRPosition().y < centerQuad.y) {
+                // une partie est en dessous
+                top = false;
+            }
+
+            if (box->getTLPosition().y > centerQuad.y) {
+                // une partie est au dessus
+                top = true;
+            }
+
+            if (top && left) {
+                topLeft->insertBox(box);
+            } else if (top) {
+                topRight->insertBox(box);
+            } else if (left) {
+                bottomLeft->insertBox(box);
+            } else {
+                bottomRight->insertBox(box);
+            }
+        }
     }
 };
 
--- include/tools/utils.hpp
+++ include/tools/utils.hpp
@@ -1,24 +1,7 @@
 #pragma once
 
-/**
- * @brief Get the padding to align center a text according to the block
- * @param blockWidth
- * @param textWidth
- * @return
- */
-int getPadding(int blockWidth, int textWidth) {
-    return (blockWidth - textWidth) / 2;
-}
+#include "glm/vec2.hpp"
 
-/**
- * @brief Get the center of a square with topLeft / bottomRight coord
- * @param topLeftCoord
- * @param bottomRightCoord
- * @return
- */
-glm::vec2 getPositionCenter(glm::vec2 topLeftCoord, glm::vec2 bottomRightCoord) {
-    return {
-            (bottomRightCoord.x - topLeftCoord.x) / 2 + topLeftCoord.x,
-            (topLeftCoord.y - bottomRightCoord.y) / 2 + bottomRightCoord.y
-    };
-}
+int getPadding(int blockWidth, int textWidth);
+
+glm::vec2 getPositionCenter(glm::vec2 topLeftCoord, glm::vec2 bottomRightCoord);
